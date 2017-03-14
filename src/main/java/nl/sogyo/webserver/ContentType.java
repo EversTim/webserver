bc3aa881550c6e12abd7f8c438bf4366ec25a752
Tim Evers <tim@sogyo.nl>
2017-03-14 14:52:31 +0100

    Fixed potential crash on getting specific half of none content type.

--- src/main/java/nl/sogyo/webserver/ContentType.java
+++ src/main/java/nl/sogyo/webserver/ContentType.java
@@ -1,7 +1,7 @@
 package nl.sogyo.webserver;
 
 public enum ContentType {
-	NONE("none"), TEXT_HTML("text/html"), TEXT_CSS("text/css"), IMAGE_JPEG("image/jpeg");
+	NONE("none/none"), TEXT_HTML("text/html"), TEXT_CSS("text/css"), IMAGE_JPEG("image/jpeg");
 
 	private String type;
 
